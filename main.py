444e2a25c37494ef5cc75e43e50b6215a62dff9d
oraja001 <oraja001@d4-w4140b-01.ib.cluster>
2026-03-30 22:06:44 -0400

    debugging

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from Network.ImageVAE import ImageVAE
 
 
 def main():
-    task = 'backdoor_attack'
+    task = 'adversarial_attack'
     label_path = "/home/oraja001/Jlab/Hydra data/labels_v2.txt"
     image_size = (640, 288)
     
