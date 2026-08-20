9b45d9a0fe9d0f760a19edbc9412a2ab579a907a
Omid Rajabi Rostami <50792481+omidrajabi6868@users.noreply.github.com>
2026-08-19 20:51:42 -0400

    Treat missing trigger softness as hard edges

--- imageattack.py
+++ imageattack.py
@@ -107,10 +107,8 @@ def main():
         f'inferred_epsilon={learned_trigger.get("effective_epsilon", learned_trigger.get("epsilon"))}'
     )
 
-    learned_softness = learned_trigger.get('softness', {})
-    selected_edge_softness = learned_softness.get(
-        'selected_edge_softness',
-        learned_softness.get('final_edge_softness', 0.2),
+    selected_edge_softness = AdversarialAttack._normalize_edge_softness(
+        learned_trigger.get('softness')
     )
     learned_how_to_attach = learned_trigger.get('how_to_attach', how_to_attach)
     selected_trigger_eval_kwargs = {
